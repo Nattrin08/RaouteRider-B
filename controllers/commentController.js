3af13ede0d3a39b1f5b6160f86de76959527e30e
Mauricio Reyes Campos <mreyesc@gmail.com>
2023-05-31 16:55:01 -0500

    API´s comment updated

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,23 +1,21 @@
 const commentModel = require('../models/commentModel');
 
 // metodo para obtener los comentarios del usuario
-exports.getUserComment = (req, res) => {
+exports.getAllComment = (req, res) => {
     const {userEmail} = req.params;
 
-    commentModel.find({userEmail: userEmail})
-    .then(comment => {
-        if(!comment) throw new Error("This user don't have comment yet")
-        res.json(comment);
-    })
-    .catch(err => res.status(500).json({error: err.message}))
+    commentModel.find()
+    .then(comment => res.json(comment))
+    .catch(err => res.status(500).json({error: err.message}));
 };
 
 // metodo para crear un comentario
 exports.createUserComment = (req, res) => {
-    const {userEmail, userComment, commentDate} = req.body;
+    const {userEmail, userComment, commentRoute, commentDate} = req.body;
     const newUser = new commentModel({
         userEmail,
         userComment,
+        commentRoute,
         commentDate
     });
 
@@ -29,9 +27,9 @@ exports.createUserComment = (req, res) => {
 // metodo para actualizar un comentario
 exports.updateUserComment = (req, res) => {
     const {id} = req.params;
-    const {userEmail, userComment} = req.body;
+    const {userEmail, userComment, commentRoute, commentDate} = req.body;
 
-    commentModel.findByIdAndUpdate(id, {userEmail, userComment}, {new: true})
+    commentModel.findByIdAndUpdate(id, {userEmail, userComment, commentRoute, commentDate}, {new: true})
     .then(comment => {
         if (!comment) throw new Error(`Comment width ${id} not found`)
         res.status(201).json(comment);
